e0f1d9751f1f6f2481f3f77dd9985a8a3cfe4002
Enzo Calvi <enzocalvi243@protonmail.com>
2022-11-09 15:02:16 -0300

    Browser router y store configurado

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect } from "react";
+import Home from "./views/Home/Products/Products";
+import { useDispatch } from "react-redux";
+import { getProducts } from "./redux/actions";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </React.Fragment>
   );
 }
 
